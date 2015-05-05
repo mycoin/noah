983dcfa96be4887ef158ae7195cac76229eaddbe
mycoin <nqliujiangtao@gmail.com>
2015-05-05 23:17:49 +0800

    remove .settings

--- src/main/java/org/ionnic/core/Interceptor.java
+++ src/main/java/org/ionnic/core/Interceptor.java
@@ -8,22 +8,18 @@ import org.springframework.web.servlet.ModelAndView;
 
 public class Interceptor implements HandlerInterceptor {
 
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("preHandle()");
+
+	}
+
+	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean preHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2) throws Exception {
+		// TODO Auto-generated method stub
 		return false;
-	}
-
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println("postHandle()");
-	}
-
-	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println("afterCompletion()");
 	}
 }
