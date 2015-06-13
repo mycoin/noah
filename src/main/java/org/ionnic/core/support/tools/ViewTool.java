032c97c4c33b12552528ad0e087e30b0b5543889
mycoin <nqliujiangtao@gmail.com>
2015-06-14 01:37:58 +0800

    update

--- src/main/java/org/ionnic/core/support/tools/ViewTool.java
+++ src/main/java/org/ionnic/core/support/tools/ViewTool.java
@@ -21,7 +21,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 public class ViewTool {
-	
+
 	public ViewTool() {
 		System.out.println(1);
 	}
