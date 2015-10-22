2d5b1fc1ea034817d25eb320524c18094f416ca7
mycoin <nqliujiangtao@gmail.com>
2015-10-22 22:50:09 +0800

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+echo "Using JAVA_HOME:       $JAVA_HOME"
+echo "Using MAVEN_HOME:      $MAVEN_HOME\n"
+
 mvn -U eclipse:clean eclipse:eclipse -DdownloadSources=true
-mvn -U clean install -DdownloadSources=true 
+mvn -U clean install -DdownloadSources=true
