dd7061ac8e9d7997660f4748dc9a037eeab18e5a
mycoin <nqliujiangtao@gmail.com>
2017-10-02 11:18:15 +0800

    feat: update

--- shared/src/main/java/com/breakidea/lotus/shared/Constants.java
+++ shared/src/main/java/com/breakidea/lotus/shared/Constants.java
@@ -6,9 +6,4 @@ public interface Constants {
 
     static final String CHARSET = "UTF-8";
 
-<<<<<<< HEAD
-	static final String CHARSET = "UTF-8";
-
-=======
->>>>>>> e226dd6841cc3dfeb355e7d2303037cec489bc81
 }
