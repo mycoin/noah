c54b3fb0e37d23556240bb9f3fa8fd9deada65f3
mycoin <nqliujiangtao@gmail.com>
2018-03-24 19:23:22 +0800

    feat: <auto-commit>

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-mvn clean install
-cd noah-web && mvn jetty:run
+
+WORKSPACE=$(cd `dirname $0`; pwd)
+cd $WORKSPACE
+
+echo "Using JAVA_HOME:       $JAVA_HOME"
+echo "Using MAVEN_HOME:      $MAVEN_HOME"
+echo "Using WORKSPACE:       $WORKSPACE"
+
+./mvnw -U clean install &&
+./mvnw -U -pl noah-web jetty:run
