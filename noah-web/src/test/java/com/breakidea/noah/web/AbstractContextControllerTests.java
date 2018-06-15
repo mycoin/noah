3445a306ffd2aa312e0ba4c9322dd3844d8a8ca1
mycoin <nqliujiangtao@gmail.com>
2018-06-16 02:37:33 +0800

    feat: <auto-commit>

--- noah-web/src/test/java/com/breakidea/noah/web/AbstractContextControllerTests.java
+++ noah-web/src/test/java/com/breakidea/noah/web/AbstractContextControllerTests.java
@@ -3,13 +3,41 @@ package com.breakidea.noah.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @WebAppConfiguration
-@ContextConfiguration(locations = { "classpath:applicationContext.xml" })
+@ContextConfiguration(locations = { TestConfig.SERVLET_XML, TestConfig.CONTEXT_XML })
 public class AbstractContextControllerTests {
 
 	@Autowired
 	protected WebApplicationContext wac;
 
+	protected MockMvc mockMvc;
+
+	protected MockMvc createMockMvc() {
+		ResultMatcher statusOk = MockMvcResultMatchers.status().isOk();
+		DefaultMockMvcBuilder builder = MockMvcBuilders.webAppContextSetup(wac);
+
+		return builder.alwaysExpect(statusOk).build();
+	}
+
+	protected MvcResult getMvcResult(MockHttpServletRequestBuilder rb) throws Exception {
+		ResultActions action = mockMvc.perform(rb);
+		MvcResult mvcResult = action.andReturn();
+
+		return mvcResult;
+	}
+
+	protected MvcResult getMvcResult(String url) throws Exception {
+		return getMvcResult(MockMvcRequestBuilders.get(url));
+	}
 }
--- noah-web/src/test/java/com/breakidea/noah/web/module/IndexControllerTests.java
+++ noah-web/src/test/java/com/breakidea/noah/web/module/IndexControllerTests.java
@@ -4,24 +4,22 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.Assert;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.breakidea.noah.web.AbstractContextControllerTests;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class IndexControllerTests extends AbstractContextControllerTests {
 
-	private MockMvc mockMvc;
-
 	@Before
 	public void setup() {
-		mockMvc = MockMvcBuilders.webAppContextSetup(wac).alwaysExpect(MockMvcResultMatchers.status().isOk()).build();
+		mockMvc = createMockMvc();
 	}
 
 	@Test
 	public void htmlView() throws Exception {
-		System.err.println(mockMvc);
+		ModelAndView mv = getMvcResult("/portal/endpoint").getModelAndView();
+		Assert.hasLength(mv.getViewName(), "");
 	}
 }
