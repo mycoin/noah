861ede1ec600ae6cc6e80ad44b44381b25690905
mycoin <nqliujiangtao@gmail.com>
2018-08-15 09:23:50 +0800

    update

--- noah-framework/src/main/java/com/breakidea/noah/framework/http/DefaultHandlerInterceptor.java
+++ noah-framework/src/main/java/com/breakidea/noah/framework/http/DefaultHandlerInterceptor.java
@@ -3,16 +3,22 @@ package com.breakidea.noah.framework.http;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class DefaultHandlerInterceptor extends HandlerInterceptorAdapter {
 
 	private static final String AJAX_REQUEST = DefaultHandlerInterceptor.class.getName();
-	
+
+	private static Log logger = LogFactory.getLog("FILE");
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		logger.info("Request DefaultHandlerInterceptor, URL:" + request.getRequestURL());
+
 		return true;
 	}
 
